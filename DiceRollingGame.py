208fba4a07c419e81b69a98ec2fc0c87e27c7654
Sammi Zafar <your-sammizafar20l@gmaile.com>
2026-09-13 15:02:41 +0530

    Initial commit

---
+++ DiceRollingGame.py
@@ -0,0 +1,11 @@
+import random 
+
+while True:
+    roll = input("Roll the dice (y/n): ")
+    if roll.lower() == 'y':
+        dice_roll = random.randint(1,6)
+        print(f"you rolled a {dice_roll}")
+    elif roll.lower() == 'n':
+        print("You chose not to roll the dice.")
+        break
+    else: print("Invalid input. Please enter 'y' to roll or 'n' to quit.")
